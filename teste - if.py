d8a69616005784ba5add190dc9899b30d4f1016a
RSerajack <78868512+RSerajack@users.noreply.github.com>
2022-04-25 14:23:42 -0300

    Add files via upload

---
+++ teste - if.py
@@ -0,0 +1,14 @@
+#Exemplos de python - if
+		
+#Vamos comparar strings, primeiro, vamos receber duas strings.
+string1 = input("digite algo: ")
+string2 = input("digite algo: ")
+#Em seguida usaremos if e else para dizer se as strings digitadas são ou não iguais.
+
+if string1 == string2:
+	print("As estradas são iguais") #caso nossa condição seja verdadeira, essa será nossa saída.
+else: 
+	print("As estradas são diferentes") #Caso a condição no if seja falsa, essa será nossa saída.
+			
+#Lembre-se, else não precisa de condição, ele executa caso a condição no if seja falsa.
+		
