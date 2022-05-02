72d4a319a78f9eead4355f30cf2b443e4d58c5d3
RSerajack <78868512+RSerajack@users.noreply.github.com>
2022-05-02 13:24:41 -0400

    Update if - for.py

--- if - for.py
+++ if - for.py
@@ -8,7 +8,7 @@ for num in lista_num:
 	cont = 0
 	for i in range(2, num):
 		if(num%i==0): cont+=1;
-	if(cont>0): print("Primo");
+	if cont == 0: print("Primo");
 	else: print("Não primo");
 
 #Saídado programa:
